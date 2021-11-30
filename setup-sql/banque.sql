9c8da898a6fe7be88cd56524f66e9af2b9792e85
sibzou <91130460+sibzou@users.noreply.github.com>
2021-11-30 11:05:27 +0100

    Création de la base de données bancaire

---
+++ setup-sql/banque.sql
@@ -0,0 +1,11 @@
+create table account (
+    num text,
+    cryptogram text,
+    balance real,
+
+    primary key(num)
+);
+
+insert into account values ("8263920393848017", "393", 100),
+                           ("2018342885251322", "897", 5),
+                           ("5352639283022076", "469", 20);
