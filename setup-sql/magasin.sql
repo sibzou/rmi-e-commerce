7bd288b8bdef735db7b0fb78071ae5c1f3641f67
kevinBoudina <81462230+kevinBoudina@users.noreply.github.com>
2021-11-30 11:44:10 +0100

    Création de la base de données du magasin

---
+++ setup-sql/magasin.sql
@@ -0,0 +1,11 @@
+create table item (
+                         id int,
+                         name text,
+                         price real,
+
+                         primary key(id)
+);
+
+insert into account values (1, "Carte SD Dell", 50),
+                           (2, "Disque dur Seagate 1To", 80),
+                           (3,"Carte graphique 3080", 999.99);
