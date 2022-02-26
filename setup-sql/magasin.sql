bf355e7045f431fc4da38170fa4c37ee478169a0
sibzou <91130460+sibzou@users.noreply.github.com>
2022-02-26 19:21:52 +0100

    Fix the shop database creation script

--- setup-sql/magasin.sql
+++ setup-sql/magasin.sql
@@ -1,11 +1,12 @@
 create table item (
-                         id int,
-                         name text,
-                         price real,
+    id int,
+    name text,
+    price real,
 
-                         primary key(id)
+    primary key(id)
 );
 
-insert into account values (1, "Carte SD Dell", 50),
-                           (2, "Disque dur Seagate 1To", 80),
-                           (3,"Carte graphique 3080", 999.99);
+-- données d'exemple
+insert into item values (1, "Carte SD Dell",          50),
+                        (2, "Disque dur Seagate 1To", 80),
+                        (3, "Carte graphique 3080",   999.99);
