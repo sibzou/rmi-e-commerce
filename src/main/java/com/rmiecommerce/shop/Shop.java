664dfa9014bdf6bb55b656eccac70cf6881b3c5c
sibzou <91130460+sibzou@users.noreply.github.com>
2022-03-05 09:12:22 +0100

    The shop server takes a port number in cmd args

--- src/main/java/com/rmiecommerce/shop/Shop.java
+++ src/main/java/com/rmiecommerce/shop/Shop.java
@@ -7,10 +7,18 @@ import java.rmi.registry.LocateRegistry;
 
 public class Shop {
     public static void main(String[] args) {
+        if(args.length == 0) {
+            System.err.println("You have to provide a listen port number");
+            System.exit(1);
+        }
+
         try {
-            LocateRegistry.createRegistry(3007);
+            LocateRegistry.createRegistry(Integer.parseInt(args[0]));
             ShopRemote shopRemote = new ShopRemote();
             Naming.rebind("rmi://localhost:3007/shop", shopRemote);
+        } catch(NumberFormatException exception) {
+            System.err.println("Incorrect port number");
+            System.exit(1);
         } catch(RemoteException | MalformedURLException ignored) {}
     }
 }
